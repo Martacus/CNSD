99dcae79a92f0b5c7313fbe3f1c472b9b60cd295
Mart <mcpkmart@hotmail.nl>
2020-09-22 11:48:43 +0200

    S

--- src/test/java/com/mart/SpringBoot/ApplicationTests.java
+++ src/test/java/com/mart/SpringBoot/ApplicationTests.java
@@ -1,13 +1,21 @@
 package com.mart.SpringBoot;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+@RunWith(JUnitPlatform.class)
 class ApplicationTests {
 
 	@Test
 	void contextLoads() {
+
 	}
 
 }
