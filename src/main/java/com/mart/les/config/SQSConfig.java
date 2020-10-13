261cd6212234a63fba93d8d04e30f39b2ded8dd9
Mart <mcpkmart@hotmail.nl>
2020-10-13 19:57:04 +0200

    ?

--- src/main/java/com/mart/les/config/SQSConfig.java
+++ src/main/java/com/mart/les/config/SQSConfig.java
@@ -1,14 +1,28 @@
 package com.mart.les.config;
 
+import com.amazon.sqs.javamessaging.AmazonSQSExtendedClient;
+import com.amazon.sqs.javamessaging.ExtendedClientConfiguration;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicSessionCredentials;
 import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.sqs.AmazonSQS;
 import com.amazonaws.services.sqs.AmazonSQSAsync;
 import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder;
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Date;
 
 @Configuration
 public class SQSConfig {
@@ -35,5 +49,20 @@ public class SQSConfig {
                 .withCredentials(new AWSStaticCredentialsProvider(
                         new BasicSessionCredentials(awsAccessKey, awsSecretKey, awsSessionToken))).build();
     }
+
+    @Bean
+    public AmazonSQS amazonSQSClient() {
+        ExtendedClientConfiguration extendedClientConfig =
+                new ExtendedClientConfiguration()
+                        .withLargePayloadSupportEnabled(AmazonS3ClientBuilder.standard().withRegion(region)
+                                .withCredentials(new AWSStaticCredentialsProvider(new BasicSessionCredentials(awsAccessKey, awsSecretKey, awsSessionToken))
+                                ).build(), "martles212");
+
+        return new AmazonSQSExtendedClient(AmazonSQSClientBuilder.defaultClient(), extendedClientConfig);
+    }
+
+
+
+
 
 }
--- src/main/java/com/mart/les/controller/SQSController.java
+++ src/main/java/com/mart/les/controller/SQSController.java
@@ -1,25 +1,37 @@
 package com.mart.les.controller;
 
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.sqs.AmazonSQS;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
+import org.springframework.http.MediaType;
 import org.springframework.messaging.support.MessageBuilder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Date;
+
+@Slf4j
 @RestController
 @RequestMapping("/sqs")
 public class SQSController {
 
     private static final Logger LOG = LoggerFactory.getLogger(SQSController.class);
 
+    private final AmazonSQS amazonSQSClient;
     private final QueueMessagingTemplate queueMessagingTemplate;
 
-    public SQSController(QueueMessagingTemplate queueMessagingTemplate) {
+    public SQSController(QueueMessagingTemplate queueMessagingTemplate, AmazonSQS amazonSQSClient) {
         this.queueMessagingTemplate = queueMessagingTemplate;
+        this.amazonSQSClient = amazonSQSClient;
     }
 
     @Value("${cloud.aws.end-point.uri}")
@@ -28,5 +40,50 @@ public class SQSController {
     @GetMapping
     public void sendMessage(){
         queueMessagingTemplate.send(sqsEndpoint, MessageBuilder.withPayload("Hello world").build());
+        LOG.info("Send message to {}", sqsEndpoint);
     }
+
+    @PostMapping(value = "/test", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+    public String gg(@RequestParam MultipartFile file){
+        return amazonSQSClient.sendMessage();
+        //Message message = queueMessagingTemplate.receive(sqsEndpoint);
+        //log.info("Recieved Message {}", message.getPayload().toString());
+    }
+
+//    private File convertMultiPartToFile(MultipartFile file) throws IOException {
+//        File convFile = new File(file.getOriginalFilename());
+//        FileOutputStream fos = new FileOutputStream(convFile);
+//        fos.write(file.getBytes());
+//        fos.close();
+//        return convFile;
+//    }
+//    private String generateFileName(MultipartFile multiPart) {
+//        return new Date().getTime() + "-" + multiPart.getOriginalFilename().replace(" ", "_");
+//    }
+//
+//    private void uploadFileTos3bucket(String fileName, File file) {
+//        amazonSQS.putObject(new PutObjectRequest("martles212", fileName, file)
+//                .withCannedAcl(CannedAccessControlList.PublicRead));
+//    }
+//
+//    public String uploadFile(MultipartFile multipartFile) {
+//
+//        String fileUrl = "";
+//        try {
+//            File file = convertMultiPartToFile(multipartFile);
+//            String fileName = generateFileName(multipartFile);
+//            fileUrl = "https://s3.eu-west-1.amazonaws.com" + "/" + "martles212" + "/" + fileName;
+//            uploadFileTos3bucket(fileName, file);
+//            file.delete();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        return fileUrl;
+//    }
+//
+//    public String deleteFileFromS3Bucket(String fileUrl) {
+//        String fileName = fileUrl.substring(fileUrl.lastIndexOf("/") + 1);
+//        amazonSQS.deleteObject(new DeleteObjectRequest( "martles212/", fileName));
+//        return "Successfully deleted";
+//    }
 }
